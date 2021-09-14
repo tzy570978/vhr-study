242d2ecca547089b6486c7671d0a2c1afbca771d
lipu <lipu@csii.com.cn>
2021-09-15 01:33:52 +0800

    登录页

--- vuehr/src/utils/validate.js
+++ vuehr/src/utils/validate.js
@@ -1,3 +1,12 @@
 /**
  * Created by PanJiaChen on 16/11/18.
  */
+
+/**
+ * @param {string} str
+ * @returns {Boolean}
+ */
+export function validUsername(str) {
+    const valid_map = ['admin', 'editor']
+    return valid_map.indexOf(str.trim()) >= 0
+}
