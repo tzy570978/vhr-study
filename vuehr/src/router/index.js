b01f1649500b7ef1979100f8afebd5cf792e2fdb
lipu <lipu@csii.com.cn>
2021-09-17 00:11:18 +0800

    格式化文件

--- vuehr/src/router/index.js
+++ vuehr/src/router/index.js
@@ -4,18 +4,18 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export const constantRoutes = [
-    {
-        path: '/login',
-        component: () => import('@/views/login/index')
-    },
-    {
-        path: '/login2',
-        component: () => import('@/views/login/index')
-    }
+  {
+    path: '/login',
+    component: () => import('@/views/login/index')
+  },
+  {
+    path: '/login2',
+    component: () => import('@/views/login/index')
+  }
 ]
 
 const router = new Router({
-    routes: constantRoutes
+  routes: constantRoutes
 })
 
 export default router
--- vuehr/src/utils/validate.js
+++ vuehr/src/utils/validate.js
@@ -7,6 +7,6 @@
  * @returns {Boolean}
  */
 export function validUsername(str) {
-    const valid_map = ['admin', 'editor']
-    return valid_map.indexOf(str.trim()) >= 0
+  const valid_map = ['admin', 'editor']
+  return valid_map.indexOf(str.trim()) >= 0
 }
