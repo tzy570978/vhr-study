e41fd7f94af117a30da2664545ba7db0284bea38
lipu <lipu@csii.com.cn>
2021-09-17 17:49:31 +0800

    全局 CLI 配置-网页标题、打版路径等

--- vuehr/vue.config.js
+++ vuehr/vue.config.js
@@ -1,25 +1,41 @@
+
 const path = require('path')
+const defaultSettings = require('./src/settings.js')
+
+const name = defaultSettings.title || "vue Element Admin"
 
 function resolve(dir) {
-    return path.join(__dirname, dir)
+  return path.join(__dirname, dir)
 }
+
 module.exports = {
-    chainWebpack(config){
-        // set svg-sprite-loader
-        config.module
-            .rule('svg')
-            .exclude.add(resolve('src/icons'))
-            .end()
-        config.module
-            .rule('icons')
-            .test(/\.svg$/)
-            .include.add(resolve('src/icons'))
-            .end()
-            .use('svg-sprite-loader')
-            .loader('svg-sprite-loader')
-            .options({
-                symbolId: 'icon-[name]'
-            })
-            .end()
-    }
+  publicPath: '/',
+  outputDir: 'distper',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+  chainWebpack(config) {
+    // set svg-sprite-loader
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+  },
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name
+
+  }
 }
